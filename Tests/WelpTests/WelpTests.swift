4ec69c9553909a9d79f40f021e042de63354c24b
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-11-13 17:16:12 -0500

    Fix broken. unused test

--- Tests/WelpTests/WelpTests.swift
+++ Tests/WelpTests/WelpTests.swift
@@ -2,10 +2,6 @@ import XCTest
 @testable import Welp
 
 final class WelpTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Welp().text, "Hello, World!")
-    }
+	func testExample() throws {
+	}
 }
